051093d13dd01decef2a30ce8a892c9d41b04406
github username <github email address>
2017-07-06 23:40:31 -0400

    Done.

---
+++ db/migrate/20170707033559_change_post_status_data_type_to_posts_2.rb
@@ -0,0 +1,5 @@
+class ChangePostStatusDataTypeToPosts2 < ActiveRecord::Migration
+  def change
+    add_column :posts, :published_status, :string
+  end
+end
---
+++ db/migrate/20170707033826_change_post_status_data_type_to_posts_3.rb
@@ -0,0 +1,5 @@
+class ChangePostStatusDataTypeToPosts3 < ActiveRecord::Migration
+  def change
+    change_column :posts, :post_status, :string
+  end
+end
